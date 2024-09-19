ed1601cb216afee079caf7c9ad4c740af424bbf3
Jehangir Kazi <kazijehangir@gmail.com>
2024-09-19 18:32:46 -0400

    Updated names to match indent level

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   docus: {
     title: 'JehangirKazi.com',
-    description: 'The curly-haired guide to curiosity ',
+    description: 'Staying Curious',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxtstudio',
@@ -9,6 +9,7 @@ export default defineAppConfig({
     },
     aside: {
       level: 0,
+      collapsed: false,
       exclude: []
     },
     header: {
