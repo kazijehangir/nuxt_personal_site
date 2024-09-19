2d4e66419caabe1b7b56f4a04bdc7004774ebb39
Jehangir Kazi <kazijehangir@users.noreply.github.com>
2024-09-19 17:40:05 -0400

    5 changes by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -12,7 +12,8 @@ export default defineAppConfig({
       exclude: []
     },
     header: {
-      logo: true
+      logo: true,
+      title: 'JehangirKazi.com'
     },
     footer: {
       iconLinks: [
